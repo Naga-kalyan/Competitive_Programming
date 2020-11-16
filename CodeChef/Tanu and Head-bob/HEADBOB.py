c1d6456ea04867d8841d6f8f3bf09e9f3773adab
Naga Kalyan <70645521+Naga-kalyan@users.noreply.github.com>
2020-11-16 08:48:00 +0530

    Create HEADBOB.py

---
+++ CodeChef/Tanu and Head-bob/HEADBOB.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    length=int(input())
+    String=input()
+    if("I" in String):
+        print("INDIAN")
+    elif("Y" in String):
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
